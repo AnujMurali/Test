72df7f083b3855470a3ca12e79411a07a3a52fb0
Anuj Murali <40844059+AnujMurali@users.noreply.github.com>
2020-05-18 21:29:07 -0500

    Update Demo_Cunit.c

--- Demo_Cunit.c
+++ Demo_Cunit.c
@@ -1,27 +1,31 @@
-#include <CUnit/CUnit.h>
-#include <CUnit/Basic.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include "CUnit/Basic.h"
+#include "CUnit/CUnit.h"
+#include "collab.h"
 
-int maxi(int i1, int i2)
+void test_mod(void)
 {
-    return (i1 > i2) ? i1 : i2;
+	CU_ASSERT (mod(3,2) == 1);
 }
 
-void test_maxi(void)
+void test_power(void)
 {
-    CU_ASSERT(maxi(0,2) == 2);
-   // CU_ASSERT(maxi(2,0) == 2);
-    CU_ASSERT(maxi(0,0) == 0);
+	CU_ASSERT (power(3,2) == 9);
+    
 }
 
-int main() {
-    CU_initialize_registry();
-    CU_pSuite suite = CU_add_suite("maxi_test", 0, 0);
-
-    CU_add_test(suite, "maxi_fun", test_maxi);
-
-    CU_basic_set_mode(CU_BRM_VERBOSE);
-    CU_basic_run_tests();
-    CU_cleanup_registry();
-
-    return 0;
+int main() 
+{
+CU_initialize_registry();
+CU_pSuite suite_1 = CU_add_suite("TestSuite_1", 0, 0);
+CU_pSuite suite_2 = CU_add_suite("TestSuite_2", 0, 0);
+CU_add_test(suite_1, "Test_Suite_1", test_mod);
+CU_add_test(suite_2, "Test_Suite_2", test_power);
+CU_basic_set_mode(CU_BRM_VERBOSE);
+CU_basic_run_tests();
+CU_cleanup_registry();
+return 0;
 }
